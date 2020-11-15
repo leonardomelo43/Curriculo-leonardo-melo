4cd21e291812558d07371a3348b8465735d51ce4
Leonardo Melo <leonardomelo43@gmail.com>
2020-11-15 15:37:07 -0300

    correçao

--- curriculo_leonardo/settings.py
+++ curriculo_leonardo/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*qig+3)ha!l_^j^x9rd=-dl&2vb0&l_$tm+l9qok8y8w)$!ou$'
-
+SECRET_KEY = ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -119,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
