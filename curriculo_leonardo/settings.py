1820cc16fa1623c3773341377be097c40671bec9
Leonardo Melo <leonardomelo43@gmail.com>
2020-11-15 18:32:12 -0300

    mod

--- curriculo_leonardo/settings.py
+++ curriculo_leonardo/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '*qig+3)ha!l_^j^x9rd=-dl&2vb0&l_$tm+l9qok8y8w)$!ou$'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == True
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://curriculo-leonardo-melo.herokuapp.com"]
 
